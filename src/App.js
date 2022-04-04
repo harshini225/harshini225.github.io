89667e6e3e5d1f7493eb6511dbea5c05bc5602c4
Harshini <harshini_venkatachalam@brown.edu>
2022-04-04 14:37:16 -0400

    test website

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import logo from './logo.svg'; 
+
+function App() {
+  return (
+    <div>
+    <div className="App">
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        <div className="sun">
+          </div>
+        </p>
+      </header>
+    </div>
+    <div className="rectangle">
+    </div>
+    </div>
+  );
+}
+
+export default App;
